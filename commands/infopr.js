67d612d79832a748a3679dec6fde70f618560db2
FinnDP2007 <56783151+FinnDP2007@users.noreply.github.com>
2020-07-12 12:39:25 +0200

    Update infopr.js

--- commands/infopr.js
+++ commands/infopr.js
@@ -3,7 +3,7 @@ const discord = require("discord.js");
 module.exports.run = async(client, message, args) => {
 var prEmbed = new discord.MessageEmbed()
 .setTitle("💵 Prijzen 💵")
-.setDescription(" \n📦Discord Servers\n━━━━━━━━━\nNormale Discord Server: €1,50\nUitgebreide Discord Server: €2,50\n🤖Discord Bots\n━━━━━━━━━\nGiveaway Bot: €3,00\n \n \n✓ Wij accepteren alleen Paypal!")
+.setDescription("\n \n📦Discord Servers\n━━━━━━━━━\nNormale Discord Server: €1,50\nUitgebreide Discord Server: €2,50\n \n🤖Discord Bots\n━━━━━━━━━\nGiveaway Bot: €3,00\n \n \n✓ Wij accepteren alleen Paypal!")
 .setColor("#78A2D2")
 
 .setTimestamp()
