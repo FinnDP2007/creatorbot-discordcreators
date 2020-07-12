3dca37abd8f585b9253a9dde21bc2e2a70dad3ec
FinnDP2007 <56783151+FinnDP2007@users.noreply.github.com>
2020-07-12 03:28:01 +0200

    Update infogc.js

--- commands/infogc.js
+++ commands/infogc.js
@@ -3,7 +3,7 @@ const discord = require("discord.js");
 module.exports.run = async(client, message, args) => {
     var gcEmbed = new discord.MessageEmbed()
     .setTitle('🎉 Giveaway Creator Bot 🎉')
-    .setDescription("Type g?help om een overzicht te krijgen van alle commands\n \n**Botinfo**\nPlubliekelijk op: 15/07/2020 BRUSSELS:12:00\nServers: ±1\n \nOm een support rol te krijgen klik dan op de emoji hieronder!")
+    .setDescription("Type g?help om een overzicht te krijgen van alle commands\n \n**Botinfo**\nPubliekelijk op: 15/07/2020 BRUSSELS:12:00\nServers: ±1\n \nOm een support rol te krijgen klik dan op de emoji hieronder!")
     .setColor("#78A2D2")
 
     .setTimestamp()
