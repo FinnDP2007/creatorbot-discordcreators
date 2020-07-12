11728c4571523c9ed7d9cf29154b276e3606039d
FinnDP2007 <56783151+FinnDP2007@users.noreply.github.com>
2020-07-12 03:09:35 +0200

    infogc-update

---
+++ commands/infogc.js
@@ -0,0 +1,18 @@
+const discord = require("discord.js");
+
+module.exports.run = async(client, message, args) => {
+    var gcEmbed = new discord.MessageEmbed()
+    .setTitle('🎉 Giveaway Creator Bot 🎉')
+    .setDescription("Type g?help om een overzicht te krijgen van alle commands\n \n**Botinfo**\nPlubliekelijk op: 15/07/2020 BRUSSELS:12:00\nServers: ±1\n \nOm een support rol te krijgen klik dan op de emoji hieronder!")
+    .setColor("#78A2D2")
+
+    .setTimestamp()
+    .setFooter('Giveaway Creator', 'https://i.imgur.com/5IbmU3W_d.webp?maxwidth=728&fidelity=grand');
+
+return message.channel.send(gcEmbed);
+
+}
+
+module.exports.help = {
+    name:("infogc")
+}
